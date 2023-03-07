182d5422f920cc6ed99232db16fb9e28895a1d9f
dev-Adward <youchanj66@gmail.com>
2023-03-07 20:38:19 +0900

    비밀번호 찾기 페이지 만들고 기능까지 구현함

--- workspace/knowhow/src/com/knowhow/member/ChangePasswordActionController.java
+++ workspace/knowhow/src/com/knowhow/member/ChangePasswordActionController.java
@@ -7,20 +7,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.knowhow.Action;
 import com.knowhow.Result;
 import com.knowhow.member.dao.MemberDAO;
+
 
 public class ChangePasswordActionController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		MemberDAO memberDAO = new MemberDAO();
+		Result result = new Result();
+		Long memberId = null;
+		String memberIdentification = null, newPassword = null;
+		System.out.println(req.getParameter("memberId"));
+//		맴버아이디를 url창에서 가져오고
+		memberId = Long.valueOf(req.getParameter("memberId"));
 		
+		memberIdentification = memberDAO.getMemberIdentification(memberId);
+		System.out.println("맴버아이디는" + memberIdentification);
 		
+		newPassword = req.getParameter("memberNewPassword");
+		System.out.println("들어온 비밀번호는" + newPassword);
 		
+		memberDAO.changePassword(memberIdentification, newPassword);
 		
-		return null;
+		result.setPath("login.member");
+		result.setRedirect(true);
+		
+		return result;
 	}
 
 }
